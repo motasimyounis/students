1f7fec8ab093152fd6b11ff2af5923eb96b217e5
Motasim T Younis <106841046+motasimyounis@users.noreply.github.com>
2023-01-04 21:38:19 +0200

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 # from dashboard import views as dash_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('student_app.urls')),
     # path('register/',dash_views.register,name='register')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
